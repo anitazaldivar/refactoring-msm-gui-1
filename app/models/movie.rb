3aa20e8ac884162b20a7715bb434d495ca21039e
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-06 15:26:20 +0000

    created the director method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,14 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def title_with_year
+    return "#{self.title} (#{self.year})"
+  end
+
+  def the_director
+    d_id = self.director_id
+    matching_directors = Director.where({ :id => d_id})
+    d = matching_directors.at(0).name
+    return d
+  end
 end
